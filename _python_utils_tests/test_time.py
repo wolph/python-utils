84de092369a5dbd38f09c11d3c6c4f4892e60ef7
Rick van Hattem <Wolph@wol.ph>
2022-01-04 14:11:36 +0100

    made tests more robust

--- _python_utils_tests/test_time.py
+++ _python_utils_tests/test_time.py
@@ -1,5 +1,5 @@
-import datetime
 import itertools
+from datetime import timedelta
 
 import pytest
 
@@ -9,19 +9,15 @@ from python_utils import time
 
 @pytest.mark.parametrize(
     'timeout,interval,interval_multiplier,maximum_interval,iterable,result', [
-        (0.01, 0.006, 0.5, 0.01, aio.acount, 2),
-        (0.01, 0.006, 0.5, 0.01, aio.acount, 2),
-        (0.01, 0.006, 0.5, 0.01, aio.acount(), 2),
-        (0.01, 0.006, 1.0, None, aio.acount, 2),
-        (datetime.timedelta(seconds=0.01),
-         datetime.timedelta(seconds=0.006),
-         2.0, datetime.timedelta(seconds=0.01), aio.acount,
-         2),
+        (0.01, 0.003, 0.5, 0.01, aio.acount, 3),
+        (0.02, 0.003, 0.5, 0.01, aio.acount(), 6),
+        (0.03, 0.003, 1.0, None, aio.acount, 9),
+        (timedelta(seconds=0.01), timedelta(seconds=0.006),
+         2.0, timedelta(seconds=0.01), aio.acount, 2),
     ])
 @pytest.mark.asyncio
-async def test_aio_timeout_generator(iterable, timeout, interval,
-                                     interval_multiplier,
-                                     maximum_interval, result):
+async def test_aio_timeout_generator(timeout, interval, interval_multiplier,
+                                     maximum_interval, iterable, result):
     i = None
     async for i in time.aio_timeout_generator(
             timeout, interval, iterable,
@@ -37,14 +33,13 @@ async def test_aio_timeout_generator(iterable, timeout, interval,
         (0.01, 0.006, 0.5, 0.01, itertools.count, 2),
         (0.01, 0.006, 0.5, 0.01, itertools.count(), 2),
         (0.01, 0.006, 1.0, None, 'abc', 'c'),
-        (datetime.timedelta(seconds=0.01),
-         datetime.timedelta(seconds=0.006),
-         2.0, datetime.timedelta(seconds=0.01),
+        (timedelta(seconds=0.01),
+         timedelta(seconds=0.006),
+         2.0, timedelta(seconds=0.01),
          itertools.count, 2),
     ])
-def test_timeout_generator(
-        iterable, timeout, interval, interval_multiplier,
-        maximum_interval, result):
+def test_timeout_generator(timeout, interval, interval_multiplier,
+                           maximum_interval, iterable, result):
     i = None
     for i in time.timeout_generator(
             timeout=timeout,
