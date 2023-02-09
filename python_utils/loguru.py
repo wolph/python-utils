2df4ac04fb6e01c8581626525cfec62db69d7463
Rick van Hattem <Wolph@wol.ph>
2023-02-09 16:14:05 +0100

    Fixed last issue of #37

--- python_utils/loguru.py
+++ python_utils/loguru.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import loguru
 
-from . import logger as logger_module
+from . import logger as logger_module, types
 
 __all__ = ['Logurud']
 
@@ -10,6 +10,6 @@ __all__ = ['Logurud']
 class Logurud(logger_module.LoggerBase):
     logger: loguru.Logger
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args: types.Any, **kwargs: types.Any) -> Logurud:
         cls.logger: loguru.Logger = loguru.logger.opt(depth=1)
         return super().__new__(cls)
