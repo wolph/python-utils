f96fbe0b2b3b52b7eac91c6e02d0ed210a681750
Rick van Hattem <Wolph@wol.ph>
2023-05-26 13:02:07 +0200

    Added AIO tests

---
+++ _python_utils_tests/test_aio.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import pytest
+import asyncio
+from python_utils.aio import acount
+
+
+@pytest.mark.asyncio
+async def test_acount(monkeypatch: pytest.MonkeyPatch):
+    sleeps: list[float] = []
+
+    async def mock_sleep(delay: float):
+        sleeps.append(delay)
+
+    monkeypatch.setattr(asyncio, 'sleep', mock_sleep)
+
+    async for i in acount(delay=1, stop=3.5):
+        print('i', i, datetime.now())
+
+    assert len(sleeps) == 4
+    assert sum(sleeps) == 4
